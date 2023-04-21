a7019bf63883ea12fab917477451cd17196b5615
Hyuzin <muhammad.libran1@gmail.com>
2023-04-21 08:26:39 +0700

    Send Email Next Js

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,178 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Montserrat } from "next/font/google";
+import { BiArrowBack } from "react-icons/bi";
+import {
+  Button,
+  ChakraProvider,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { sendContactForm } from "../lib/api";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
+const initValues = { name: "", email: "", subject: "", message: "" };
 
-export default function Home() {
+const initState = { isLoading: false, error: "", values: initValues };
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+const Home = () => {
+  const toast = useToast();
+  const [state, setState] = useState(initState);
+  const [touched, setTouched] = useState({});
+
+  const { values, isLoading, error } = state;
+
+  const onBlur = ({ target }) =>
+    setTouched((prev) => ({ ...prev, [target.name]: true }));
+
+  const handleChange = ({ target }) =>
+    setState((prev) => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [target.name]: target.value,
+      },
+    }));
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    try {
+      await sendContactForm(values);
+      setTouched({});
+      setState(initState);
+      toast({
+        title: "Pesan terkirim.",
+        status: "success",
+        duration: 2000,
+        position: "top",
+      });
+    } catch (error) {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        error: error.message,
+      }));
+    }
+  };
+
+  const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <ChakraProvider>
+      <div
+        className={`${montserrat.className} bg-gray-300 flex justify-center items-center  md:px-[12%] lg:px-[25%] w-screen h-screen`}
+      >
+        <div className="flex flex-col w-full gap-5 py-[50%] md:py-10 px-5 bg-white rounded-xl ">
+          <div className="flex flex-row items-center">
+            <Button
+              variant='outline'
+              className="cursor-pointer"
+              marginRight={10}
+              type="button"
+              borderColor='red.500'
+              color='red.500'
+              _hover={{ bgColor: 'red.500', color: 'white' }}
+              onClick={() => router.back()}
+            >
+              <BiArrowBack />
+            </Button>
+            <p className="font-semibold text-center text-red-500">
+              KIRIM PESAN
+            </p>
+          </div>
+          {error && (
+            <Text color="red.500" fontSize="md">
+              {error}
+            </Text>
+          )}
+          <FormControl isRequired isInvalid={touched.name && !values.name}>
+            <FormLabel>Nama</FormLabel>
+            <Input
+              type="text"
+              name="name"
+              errorBorderColor="red.500"
+              value={values.name}
+              onChange={handleChange}
+              focusBorderColor="black"
+              onBlur={onBlur}
             />
-          </a>
+            <FormErrorMessage>Input wajib diisi.</FormErrorMessage>
+          </FormControl>
+          <FormControl isRequired isInvalid={touched.email && !values.email}>
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="text"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              focusBorderColor="black"
+              errorBorderColor="red.500"
+              onBlur={onBlur}
+            />
+            <FormErrorMessage>Input wajib diisi.</FormErrorMessage>
+          </FormControl>
+          <FormControl
+            isRequired
+            isInvalid={touched.subject && !values.subject}
+          >
+            <FormLabel>Subjek</FormLabel>
+            <Input
+              type="text"
+              name="subject"
+              value={values.subject}
+              onChange={handleChange}
+              focusBorderColor="black"
+              errorBorderColor="red.500"
+              onBlur={onBlur}
+            />
+            <FormErrorMessage>Input wajib diisi.</FormErrorMessage>
+          </FormControl>
+          <FormControl
+            isRequired
+            isInvalid={touched.message && !values.message}
+          >
+            <FormLabel>Pesan</FormLabel>
+            <Textarea
+              name="message"
+              value={values.message}
+              onChange={handleChange}
+              rows={3}
+              onBlur={onBlur}
+              focusBorderColor="black"
+              errorBorderColor="red.500"
+            />
+            <FormErrorMessage>Input wajib diisi.</FormErrorMessage>
+          </FormControl>
+          <Button
+            variant="outline"
+            isLoading={isLoading}
+            bgColor="red.500"
+            color="white"
+            disabled={
+              !values.name ||
+              !values.email ||
+              !values.subject ||
+              !values.message
+            }
+            _hover={{ bgColor: "white", color: "red.500" }}
+            _active={{ bgColor: "red.700", color: "white" }}
+            onClick={onSubmit}
+          >
+            Kirim
+          </Button>
         </div>
       </div>
+    </ChakraProvider>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
